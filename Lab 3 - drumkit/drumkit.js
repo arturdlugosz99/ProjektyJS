0befbb791e55ea7dd9cf8e7175c54df615f15787
Artur <artur.dlugosz@microsoft.wsei.edu.pl>
2024-02-09 16:37:14 +0100

    v1

--- Lab 3 - drumkit/drumkit.js
+++ Lab 3 - drumkit/drumkit.js
@@ -1,15 +1,106 @@
-document.addEventListener('keypress', onKeyPress)
+document.addEventListener('keypress', onKeyPress);
 
-const KeyToSound = {
-    'a': document.querySelector('#s1'),
-    's': document.querySelector('#s2')
+let records = [];
+let isRecording = false;
+let record = [];
+
+class Sound {
+    constructor(key) {
+        this.key = key;
+        this.timestamp = Date.now();
+    }
 }
 
 function onKeyPress(event) {
-    const sound = KeyToSound[event.key]
-    playSound(sound)
+    const key = event.key;
+    let sound = '';
+    const keyElement = document.querySelector(`#${key}`);
+    
+    switch (key) {
+        case 'q':
+            sound = 'boom';
+            break;
+        case 'w':
+            sound = 'tom';
+            break;
+        case 'e':
+            sound = 'hihat';
+            break;
+        case 'a':
+            sound = 'kick';
+            break;
+        case 's':
+            sound = 'openhat';
+            break;
+        case 'd':
+            sound = 'ride';
+            break;
+        case 'z':
+            sound = 'snare';
+            break;
+        case 'x':
+            sound = 'clap';
+            break;
+        case 'c':
+            sound = 'tink';
+            break;
+        default:
+            sound = 'default';
+            break;
+    }
+
+    keyElement.classList.add('keyPressed');
+    setTimeout(() => {
+        keyElement.classList.remove('keyPressed');
+    }, 100);
+
+    if (isRecording) record.push(new Sound(sound));
+    playSound(sound);
 }
+
 function playSound(sound) {
-    sound.currentTime = 0
-    sound.play()
+    const audioTag = document.querySelector(`#${sound}`);
+    audioTag.currentTime = 0;
+    audioTag.play();
 }
+
+function playRecord(record) {
+    record.forEach((sound) => {
+        setTimeout(() => {
+            playSound(sound.key);
+        }, sound.timestamp - record[0].timestamp);
+    });
+}
+
+function showRecords() {
+    const recordsList = document.querySelector('.records');
+    recordsList.innerHTML = '';
+    records.forEach((record, index) => {
+        const li = document.createElement('li');
+        li.classList.add('record');
+        li.textContent = `Record ${index + 1}`;
+        li.addEventListener('click', () => {
+            if (isRecording) {
+                alert("Nie można odtwarzać podczas nagrywania.");
+            } else {
+                playRecord(record);
+            }
+        });
+        recordsList.appendChild(li);
+    });
+}
+
+const recordButton = document.querySelector('#record');
+recordButton.addEventListener('click', () => {
+    isRecording = !isRecording;
+    if (isRecording) {
+        recordButton.textContent = 'Zatrzymaj nagrywanie';
+        recordButton.classList.add('recording');
+    } else {
+        recordButton.textContent = 'Nagrywaj';
+        recordButton.classList.remove('recording');
+        records.push(record);
+        record = [];
+        showRecords();
+    }
+});
